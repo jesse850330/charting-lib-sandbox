72458221b800802e95d546d0e3b2dc13aed07af6
Verstarker <hayden.wrathall@gmail.com>
2018-01-25 15:23:09 +1300

    added extra data to graph and renamed js file

--- projects/milestone-projects/students-demographics-team17/projects/vis-hayden/js/main.js
+++ projects/milestone-projects/students-demographics-team17/projects/vis-hayden/js/main.js
@@ -2,14 +2,16 @@ var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Spark', 'Vodafone', '2Degrees'],
+                labels: ['Spark', 'Vodafone', '2Degrees', 'Slingshot', 'Other'],
                 datasets: [{
                     label: 'Data',
-                    data: [3, 3, 2],
+                    data: [3, 3, 2, 0, 0],
                     borderColor: 'rgba(255, 0, 0, 0.6)',
                     backgroundColor: ['rgba(255, 99, 82, 0.5)',
                                     'rgba(54, 162, 235, 0.5)',
-                                    'rgba(255, 206, 86, 0.5)'],
+                                    'rgba(255, 206, 86, 0.5)',
+                                    'rgba(75, 192, 192, 0.5)',
+                                    'rgba(153, 102, 255, 0.5)',],
                 }],
             },
             options: {
